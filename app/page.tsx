55b3c4bc2e9f61ed6d3e973cf597bc255e570327
iJack80 <work.davidmainoo@gmail.com>
2023-10-18 22:16:58 +0000

    Build home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,30 @@
 import Navbar from "@/components/Navbar";
 import Image from "next/image";
+import EngineerImg from "../public/illustrations/engineer.svg";
+import Footer from "@/components/Footer";
 
 export default function Home() {
     return (
         <>
             <Navbar />
-            <main className="">
+            <main className="mt-10 mb-20">
                 <Image
-                    src={"/illustrations/engineer.svg"}
-                    alt=" Illustration of an engineer"
-                    width={228}
-                    height={228}
-                    className="w-60"
-                    // placeholder="blur"
+                    src={EngineerImg}
+                    alt="illustration of an engineer"
+                    className="w-60 md:w-72 my-4"
+                    priority={true}
                 />
+                <h4 className="font-semibold  text-gray-500">Hi there, I’m </h4>
+                <h1 className="font-bold text-4xl text-gray-600">David Mainoo,</h1>
+                <p className="text-gray-600 mt-2">
+                    Feast your eyes on my portfolio and when you are done, give it a star
+                    on github
+                </p>
             </main>
+            <Footer
+                linkName="About"
+                linkUrl="/about"
+            />
         </>
     );
 }
